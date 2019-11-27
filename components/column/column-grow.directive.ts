de7ec028f5a6e64f6f60cdcb5a48db04553637c0
Dariusz Filipiak <dariusz.filipiak@igt.com>
2019-11-27 21:11:15 +0100

    in progress new deps

--- components/column/column-grow.directive.ts
+++ components/column/column-grow.directive.ts
@@ -1,16 +1,16 @@
-import {Component, Directive, ElementRef, Renderer} from '@angular/core';
+import { Component, Directive, ElementRef, Renderer2 } from '@angular/core';
 
 
 @Directive({
-    selector: '[grow]'
+  selector: '[grow]'
 })
 export class DirectiveGrow {
-    constructor(public e: ElementRef, public renderer: Renderer) {
-        setTimeout(() => {
-            // e.nativeElement.style.flexGrow = 1;
-            renderer.setElementStyle(e.nativeElement, 'flexGrow', '1');
-        }, 0);
+  constructor(public e: ElementRef, public renderer: Renderer2) {
+    setTimeout(() => {
+      // e.nativeElement.style.flexGrow = 1;
+      renderer.setStyle(e.nativeElement, 'flexGrow', '1')
+    }, 0);
 
-        // renderer.setElementStyle(e.nativeElement, 'flexGrow', '1');
-    }
+    // renderer.setElementStyle(e.nativeElement, 'flexGrow', '1');
+  }
 }
