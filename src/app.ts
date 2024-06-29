6612e24a81c6954363ecf780a4ed17a77c158edb
Dariusz Filipiak <darekf77@gmail.com>
2024-06-29 14:21:52 +0200

    $$$ update $$$

--- src/app.ts
+++ src/app.ts
@@ -1,89 +1,120 @@
-// //#region @notForNpm
-// //#region imports
-// import { Firedev } from 'firedev';
-// import { HOST_BACKEND_PORT } from './app.hosts';
-// //#region @browser
-// import { NgModule, NgZone, ViewEncapsulation } from '@angular/core';
-// import { Component, OnInit } from '@angular/core';
-// import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+//#region imports
+import { Firedev, BaseContext } from 'firedev/src';
+import { Observable, map } from 'rxjs';
+import { HOST_BACKEND_PORT } from './app.hosts';
+//#region @browser
+import { NgModule, inject, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//#endregion
+//#endregion
 
-// //#endregion
-// //#endregion
+console.log('hello world');
+console.log('Your server will start on port '+ HOST_BACKEND_PORT);
+const host = 'http://localhost:' + HOST_BACKEND_PORT;
 
-// //#region @browser
+//#region static-columns component
+//#region @browser
+@Component({
+  selector: 'app-static-columns',
+  template: `hello from static-columns<br>
+    <br>
+    users from backend
+    <ul>
+      <li *ngFor="let user of (users$ | async)"> {{ user | json }} </li>
+    </ul>
+  `,
+  styles: [` body { margin: 0px !important; } `],
+})
+export class StaticColumnsComponent {
+  userApiService = inject(UserApiService);
+  readonly users$: Observable<User[]> = this.userApiService.getAll();
+}
+//#endregion
+//#endregion
 
-// //#region routes
-// const routes: Routes = [
-//   {
-//     path: '',
-//     loadChildren: () => import('./app/preview/preview.module')
-//       .then(m => m.PreviewModule),
-//   },
-// ];
-// //#endregion
+//#region  static-columns api service
+//#region @browser
+@Injectable({
+  providedIn:'root'
+})
+export class UserApiService {
+  userControlller = Firedev.inject(()=> MainContext.get(UserController))
+  getAll() {
+    return this.userControlller.getAll()
+      .received
+      .observable
+      .pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+//#endregion
 
-// //#region main component
-// @Component({
-//   selector: 'app-static-columns',
-//   encapsulation: ViewEncapsulation.None,
-//   styleUrls: ['./app.scss'],
-//   templateUrl: './app.html',
-// })
-// export class StaticColumnsComponent {
-//   constructor(
-//     private ngZone: NgZone
-//   ) { }
+//#region  static-columns module
+//#region @browser
+@NgModule({
+  exports: [StaticColumnsComponent],
+  imports: [CommonModule],
+  declarations: [StaticColumnsComponent],
+})
+export class StaticColumnsModule { }
+//#endregion
+//#endregion
 
-// }
-// //#endregion
+//#region  static-columns entity
+@Firedev.Entity({ className: 'User' })
+class User extends Firedev.Base.AbstractEntity {
+  public static ctrl?: UserController;
+  //#region @websql
+  @Firedev.Orm.Column.String()
+  //#endregion
+  name?: string;
+}
+//#endregion
 
-// //#region main module
-// @NgModule({
-//   imports: [
-//     RouterModule.forRoot(routes, {
-//       useHash: true,
-//       preloadingStrategy: PreloadAllModules,
-//       enableTracing: false,
-//     }),
-//   ],
-//   exports: [StaticColumnsComponent],
-//   declarations: [StaticColumnsComponent],
-//   providers: [],
-// })
-// export class StaticColumnsModule { }
-// //#endregion
-// //#endregion
+//#region  static-columns controller
+@Firedev.Controller({ className: 'UserController' })
+class UserController extends Firedev.Base.CrudController<User> {
+  entityClassResolveFn = ()=> User;
+  //#region @websql
+  async initExampleDbData(): Promise<void> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.db.save(superAdmin);
+  }
+  //#endregion
+}
+//#endregion
 
-// //#region firedev start function
-// async function start() {
-//   const host = `http://localhost:${HOST_BACKEND_PORT}`;
-//   // Firedev.enableProductionMode();
+//#region  static-columns context
+const MainContext = Firedev.createContext(()=>({
+  host,
+  contextName: 'MainContext',
+  contexts:{ BaseContext },
+  controllers: {
+    UserController,
+    // PUT FIREDEV CONTORLLERS HERE
+  },
+  entities: {
+    User,
+    // PUT FIREDEV ENTITIES HERE
+  },
+  database: true,
+  disabledRealtime: true,
+}));
+//#endregion
 
-//   const context = await Firedev.init({
-//     host,
-//     controllers: [
-//       // PUT FIREDEV CONTORLLERS HERE
-//     ],
-//     entities: [
-//       // PUT FIREDEV ENTITIES HERE
-//     ],
-//     //#region @websql
-//     config: {
-//       type: 'better-sqlite3',
-//       database: 'tmp-db.sqlite',
-//       logging: false,
-//     }
-//     //#endregion
-//   });
-//   //#region @backend
-//   // if (Firedev.isNode) {
-//   //   context.node.app.get('/hello', (req, res) => {
-//   //     res.send('Hello static-columns')
-//   //   })
-//   // }
-//   //#endregion
-// }
-// //#endregion
+async function start() {
 
-// export default start;
-// //#endregion
+  await MainContext.initialize();
+
+  if (Firedev.isBrowser) {
+    const users = (await MainContext.getClassInstance(UserController).getAll().received)
+      .body?.json;
+    console.log({
+      'users from backend': users,
+    });
+  }
+}
+
+export default start;
