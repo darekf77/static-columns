5be903eaee788f14c4f48d1e251a5222524636a4
Dariusz Filipiak <darekf77@gmail.com>
2025-01-06 23:22:12 +0100

    $$$ update $$$

---
+++ src/migrations/1736195079865_asd.ts
@@ -0,0 +1,22 @@
+import { Taon } from 'taon';
+import { QueryRunner } from 'taon-typeorm';
+
+@Taon.Migration({
+  className: 'MainContext_1736195079865_asd',
+})
+export class MainContext_1736195079865_asd extends Taon.Base.Migration {
+  /**
+   * remove this method if you are ready to run this migration
+   */
+  public isReadyToRun(): boolean {
+    return false;
+  }
+
+  async up(queryRunner: QueryRunner): Promise<any> {
+    // do "something" in db
+  }
+
+  async down(queryRunner: QueryRunner): Promise<any> {
+    // revert this "something" in db
+  }
+}
---
+++ src/migrations/1736195172422_myNewDbChange.ts
@@ -0,0 +1,23 @@
+import { Taon } from 'taon';
+import { QueryRunner } from 'taon-typeorm';
+
+@Taon.Migration({
+  className: 'MainContext_1736195172422_myNewDbChange',
+})
+export class MainContext_1736195172422_myNewDbChange extends Taon.Base
+  .Migration {
+  /**
+   * remove this method if you are ready to run this migration
+   */
+  public isReadyToRun(): boolean {
+    return false;
+  }
+
+  async up(queryRunner: QueryRunner): Promise<any> {
+    // do "something" in db
+  }
+
+  async down(queryRunner: QueryRunner): Promise<any> {
+    // revert this "something" in db
+  }
+}
