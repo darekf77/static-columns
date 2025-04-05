8146b2f66c2a9d544d099c307abfd8876f50737b
Dariusz Filipiak <darekf77@gmail.com>
2025-04-05 15:16:27 +0200

    $$$ update $$$

---
+++ env.ts
@@ -0,0 +1,9 @@
+// import type { EnvOptions } from 'taon/src';
+
+const env: any = {
+  website: {
+    domain: 'static-columns.example.domain.com',
+    title: 'Static Columns',
+  },
+};
+export default env;
