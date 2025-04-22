1b29d519c3e2c2b613621952c50d60540664f634
Dariusz Filipiak <darekf77@gmail.com>
2025-04-22 16:23:14 +0200

    chore: update

---
+++ src/migrations/migrations_index._auto-generated_.ts
@@ -0,0 +1,13 @@
+// THIS FILE IS GENERATED - DO NOT MODIFY
+import { MainContext_1736195079865_asd } from './1736195079865_asd';
+import { MainContext_1736195172422_myNewDbChange } from './1736195172422_myNewDbChange';
+import { MainContext_1736195659183_myNewDbChange222 } from './1736195659183_myNewDbChange222';
+import { MainContext_1736195693350_addingFlowers } from './1736195693350_addingFlowers';
+// THIS FILE IS GENERATED - DO NOT MODIFY
+export const MIGRATIONS_CLASSES_FOR_MainContext = {
+  /* 06-01-2025 21:24:39 */ MainContext_1736195079865_asd,
+  /* 06-01-2025 21:26:12 */ MainContext_1736195172422_myNewDbChange,
+  /* 06-01-2025 21:34:19 */ MainContext_1736195659183_myNewDbChange222,
+  /* 06-01-2025 21:34:53 */ MainContext_1736195693350_addingFlowers,
+};
+// THIS FILE IS GENERATED - DO NOT MODIFY
