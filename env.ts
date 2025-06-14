7739c237add696d6db89003692c6d6def6f8aa1d
Dariusz Filipiak <darekf77@gmail.com>
2025-06-14 15:39:42 +0200

    release: v19.0.28

--- env.ts
+++
@@ -1,8 +0,0 @@
-import type { EnvOptions } from 'tnp/src';
-const env: Partial<EnvOptions> = {
-  website: {
-    domain: 'static-columns.example.domain.com',
-    title: 'Static Columns',
-  },
-};
-export default env;
