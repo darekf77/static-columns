703b29ac3176c555af6e1f2dafeca8662204b462
Dariusz Filipiak <darekf77@gmail.com>
2026-01-14 21:12:16 +0100

    release: v21.0.17

---
+++ browser/chunk-D7MFVXJV.js
@@ -0,0 +1,1 @@
+import{e}from"./chunk-VB56BUGO.js";var p=e((s,t)=>{t.exports=XMLHttpRequest});export default p();
---
+++ browser/chunk-QI3QL3TY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WXE6BMS3.js";import"./chunk-VB56BUGO.js";export default a();
