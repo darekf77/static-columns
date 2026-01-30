dc19f7c333510b661ac4fd9ce99cdaaa9e21fabb
Dariusz Filipiak <darekf77@gmail.com>
2026-01-30 16:42:30 +0100

    chore: api update

--- src/app.ts
+++ src/app.ts
@@ -1,334 +1,381 @@
-// // @ts-nocheck
-// //#region imports
-// import * as os from 'os'; // @backend
+//#region imports
+import * as os from 'os'; // @backend
 
-// import { AsyncPipe, JsonPipe, NgFor } from '@angular/common'; // @browser
-// import {
-//   inject,
-//   Injectable,
-//   APP_INITIALIZER,
-//   ApplicationConfig,
-//   provideBrowserGlobalErrorListeners,
-//   isDevMode,
-//   mergeApplicationConfig,
-//   provideZonelessChangeDetection,
-//   signal,
-// } from '@angular/core'; // @browser
-// import { Component } from '@angular/core'; // @browser
-// import { VERSION } from '@angular/core'; // @browser
-// import {
-//   provideClientHydration,
-//   withEventReplay,
-// } from '@angular/platform-browser';
-// import {
-//   provideRouter,
-//   Router,
-//   RouterLinkActive,
-//   RouterModule,
-//   RouterOutlet,
-//   ActivatedRoute,
-//   Routes,
-//   Route,
-// } from '@angular/router';
-// import { provideServiceWorker } from '@angular/service-worker';
-// import { provideServerRendering, withRoutes } from '@angular/ssr';
-// import { RenderMode, ServerRoute } from '@angular/ssr';
-// import Aura from '@primeng/themes/aura'; // @browser
-// import { providePrimeNG } from 'primeng/config'; // @browser
-// import { toSignal } from '@angular/core/rxjs-interop'; // @browser
-// import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
-// import { MatCardModule } from '@angular/material/card'; // @browser
-// import { MatIconModule } from '@angular/material/icon'; // @browser
-// import { MatDividerModule } from '@angular/material/divider'; // @browser
-// import { MatButtonModule } from '@angular/material/button'; // @browser
-// import { MatListModule } from '@angular/material/list'; // @browser
-// import { MatTabsModule } from '@angular/material/tabs'; // @browser
-// import {
-//   Taon,
-//   TaonBaseContext,
-//   TAON_CONTEXT,
-//   EndpointContext,
-//   TaonBaseAngularService,
-//   TaonEntity,
-//   StringColumn,
-//   TaonBaseAbstractEntity,
-//   TaonBaseCrudController,
-//   TaonController,
-//   GET,
-//   TaonMigration,
-//   TaonBaseMigration,
-// } from 'taon/src';
-// import { Utils, UtilsOs } from 'tnp-core/src';
+import { AsyncPipe, JsonPipe, NgFor } from '@angular/common'; // @browser
+import {
+  inject,
+  Injectable,
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  isDevMode,
+  mergeApplicationConfig,
+  provideZonelessChangeDetection,
+  signal,
+} from '@angular/core'; // @browser
+import { Component } from '@angular/core'; // @browser
+import { VERSION, OnInit } from '@angular/core'; // @browser
+import { toSignal } from '@angular/core/rxjs-interop'; // @browser
+import { MatButtonModule } from '@angular/material/button'; // @browser
+import { MatCardModule } from '@angular/material/card'; // @browser
+import { MatDividerModule } from '@angular/material/divider'; // @browser
+import { MatIconModule } from '@angular/material/icon'; // @browser
+import { MatListModule } from '@angular/material/list'; // @browser
+import { MatTabsModule } from '@angular/material/tabs'; // @browser
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {
+  provideRouter,
+  Router,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+  ActivatedRoute,
+  Routes,
+  Route,
+  withHashLocation,
+} from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { RenderMode, ServerRoute } from '@angular/ssr';
+import Aura from '@primeng/themes/aura'; // @browser
+import { providePrimeNG } from 'primeng/config'; // @browser
+import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
+import {
+  Taon,
+  TaonBaseContext,
+  TAON_CONTEXT,
+  EndpointContext,
+  TaonBaseAngularService,
+  TaonEntity,
+  StringColumn,
+  TaonBaseAbstractEntity,
+  TaonBaseCrudController,
+  TaonController,
+  GET,
+  TaonMigration,
+  TaonBaseMigration,
+} from 'taon/src';
+import { Utils, UtilsOs } from 'tnp-core/src';
 
-// import { HOST_CONFIG } from './app.hosts';
-// // @placeholder-for-imports
+import { HOST_CONFIG } from './app.hosts';
+// @placeholder-for-imports
 
-// //#endregion
+//#endregion
 
-// const firstHostConfig = (Object.values(HOST_CONFIG) || [])[0];
-// console.log('Your backend host ' + firstHostConfig?.host);
-// console.log('Your frontend host ' + firstHostConfig?.frontendHost);
+const firstHostConfig = (Object.values(HOST_CONFIG) || [])[0];
+console.log('Your backend host ' + firstHostConfig?.host);
+console.log('Your frontend host ' + firstHostConfig?.frontendHost);
 
-// //#region static-columns component
+//#region static-columns component
 
-// //#region @browser
-// @Component({
-//   selector: 'app-root',
+//#region @browser
+@Component({
+  selector: 'app-root',
 
-//   imports: [
-//     // RouterOutlet,
-//     // AsyncPipe,
-//     MatCardModule,
-//     MatIconModule,
-//     MatDividerModule,
-//     MatButtonModule,
-//     MatListModule,
-//     MatTabsModule,
-//     RouterModule,
-//     // JsonPipe,
-//   ],
-//   template: `
-//     @if (itemsLoaded()) {
-//       <router-outlet />
-//     }
-//   `,
-// })
-// export class StaticColumnsApp {
-//   itemsLoaded = signal(false);
-//   navItems =
-//     StaticColumnsClientRoutes.length <= 1
-//       ? []
-//       : StaticColumnsClientRoutes.filter(r => r.path !== undefined).map(r => ({
-//           path: r.path === '' ? '/' : `/${r.path}`,
-//           label: r.path === '' ? 'Home' : `${r.path}`,
-//         }));
+  imports: [
+    // RouterOutlet,
+    AsyncPipe,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatListModule,
+    MatTabsModule,
+    RouterModule,
+    JsonPipe,
+  ],
+  template: ` <router-outlet />`,
+})
+export class StaticColumnsApp implements OnInit {
+  itemsLoaded = signal(false);
 
-//   activatedRoute = inject(ActivatedRoute);
+  navItems =
+    StaticColumnsClientRoutes.length <= 1
+      ? []
+      : StaticColumnsClientRoutes.filter(r => r.path !== undefined).map(r => ({
+          path: r.path === '' ? '/' : `/${r.path}`,
+          label: r.path === '' ? 'Home' : `${r.path}`,
+        }));
 
-//   get activePath(): string {
-//     return globalThis?.location.pathname?.split('?')[0];
-//   }
+  activatedRoute = inject(ActivatedRoute);
 
-//   ngOnInit(): void {
-//     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-//     //Add 'implements OnInit' to the class.
-//     console.log(globalThis?.location.pathname);
-//     // TODO set below from 1000 to zero in production
-//     Taon.removeLoader(1000).then(() => {
-//       this.itemsLoaded.set(true);
-//     });
-//   }
+  get activePath(): string {
+    return globalThis?.location.pathname?.split('?')[0];
+  }
 
-//   taonMode = UtilsOs.isRunningInWebSQL() ? 'websql' : 'normal nodejs';
-//   angularVersion = VERSION.full;
-//   userApiService = inject(UserApiService);
-//   router = inject(Router);
-// }
-// //#endregion
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(globalThis?.location.pathname);
+    // TODO set below from 1000 to zero in production
+    Taon.removeLoader(1000).then(() => {
+      this.itemsLoaded.set(true);
+    });
+  }
 
-// //#endregion
+  taonMode = UtilsOs.isRunningInWebSQL() ? 'websql' : 'normal nodejs';
 
-// //#region  static-columns api service
+  angularVersion = VERSION.full;
 
-// //#region @browser
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class UserApiService extends TaonBaseAngularService {
-//   userController = this.injectController(UserController);
+  userApiService = inject(UserApiService);
 
-//   getAll(): Observable<User[]> {
-//     return this.userController
-//       .getAll()
-//       .request()
-//       .observable.pipe(map(r => r.body.json));
-//   }
-// }
-// //#endregion
+  router = inject(Router);
 
-// //#endregion
+  private refresh = new BehaviorSubject<void>(undefined);
 
-// //#region  static-columns routes
-// //#region @browser
-// export const StaticColumnsServerRoutes: ServerRoute[] = [
-//   {
-//     path: '**',
-//     renderMode: RenderMode.Prerender,
-//   },
-// ];
-// export const StaticColumnsClientRoutes: Routes = [
-//   {
-//     path: '',
-//     pathMatch: 'full',
-//     redirectTo: () => {
-//       if (StaticColumnsClientRoutes.length === 1) {
-//         return '';
-//       }
-//       return StaticColumnsClientRoutes.find(r => r.path !== '')!.path!;
-//     },
-//   },
-//   // PUT ALL ROUTES HERE
-//   // @placeholder-for-routes
-//   // @app-ts-generated
-//   {
-//     path: 'preview',
+  readonly users = toSignal(
+    this.refresh.pipe(
+      switchMap(() =>
+        this.userApiService.userController
+          .getAll()
+          .request()
+          .observable.pipe(map(r => r.body.json)),
+      ),
+    ),
+    { initialValue: [] },
+  );
 
-//     loadChildren: () =>
-//       import('./app/preview/preview.routes').then(m => m.PreviewRoutes),
-//   },
-// ];
-// //#endregion
-// //#endregion
+  readonly hello$ = this.userApiService.userController
+    .helloWorld()
+    .request()
+    .observable.pipe(map(r => r.body.text));
 
-// //#region  static-columns app configs
-// //#region @browser
-// export const StaticColumnsAppConfig: ApplicationConfig = {
-//   providers: [
-//     provideZonelessChangeDetection(),
-//     {
-//       provide: TAON_CONTEXT,
-//       useFactory: () => StaticColumnsContext,
-//     },
-//     providePrimeNG({
-//       theme: {
-//         preset: Aura,
-//       },
-//     }),
-//     {
-//       provide: APP_INITIALIZER,
-//       multi: true,
-//       useFactory: () => StaticColumnsStartFunction,
-//     },
-//     provideBrowserGlobalErrorListeners(),
-//     provideRouter(StaticColumnsClientRoutes),
-//     provideClientHydration(withEventReplay()),
-//     provideServiceWorker('ngsw-worker.js', {
-//       enabled: !isDevMode(),
-//       registrationStrategy: 'registerWhenStable:30000',
-//     }),
-//   ],
-// };
+  async deleteUser(userToDelete: User): Promise<void> {
+    await this.userApiService.userController
+      .deleteById(userToDelete.id)
+      .request();
+    this.refresh.next();
+  }
 
-// export const StaticColumnsServerConfig: ApplicationConfig = {
-//   providers: [provideServerRendering(withRoutes(StaticColumnsServerRoutes))],
-// };
+  async addUser(): Promise<void> {
+    const newUser = new User();
+    newUser.name = `user-${Math.floor(Math.random() * 1000)}`;
+    await this.userApiService.userController.save(newUser).request();
+    this.refresh.next();
+  }
 
-// export const StaticColumnsConfig = mergeApplicationConfig(
-//   StaticColumnsAppConfig,
-//   StaticColumnsServerConfig,
-// );
-// //#endregion
-// //#endregion
+  forceShowBaseRootApp = false;
 
-// //#region  static-columns entity
-// @TaonEntity({ className: 'User' })
-// class User extends TaonBaseAbstractEntity {
-//   //#region @websql
-//   @StringColumn()
-//   //#endregion
-//   name?: string;
+  navigateTo(item: { path: string; label: string }): void {
+    if (item.path === '/') {
+      if (this.forceShowBaseRootApp) {
+        return;
+      }
+      this.forceShowBaseRootApp = true;
+      return;
+    }
+    this.forceShowBaseRootApp = false;
+    this.router.navigateByUrl(item.path);
+  }
+}
+//#endregion
 
-//   getHello(): string {
-//     return `hello ${this.name}`;
-//   }
-// }
-// //#endregion
+//#endregion
 
-// //#region  static-columns controller
-// @TaonController({ className: 'UserController' })
-// class UserController extends TaonBaseCrudController<User> {
-//   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-//   entityClassResolveFn = () => User;
+//#region  static-columns api service
 
-//   @GET()
-//   helloWorld(): Taon.Response<string> {
-//     //#region @websqlFunc
-//     return async (req, res) => 'hello world';
-//     //#endregion
-//   }
+//#region @browser
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends TaonBaseAngularService {
+  userController = this.injectController(UserController);
 
-//   @GET()
-//   getOsPlatform(): Taon.Response<string> {
-//     //#region @websqlFunc
-//     return async (req, res) => {
-//       //#region @backend
-//       return os.platform(); // for normal nodejs backend return real value
-//       //#endregion
+  getAll(): Observable<User[]> {
+    return this.userController
+      .getAll()
+      .request()
+      .observable.pipe(map(r => r.body.json));
+  }
+}
+//#endregion
 
-//       return 'no-platform-inside-browser-and-websql-mode';
-//     };
-//     //#endregion
-//   }
-// }
-// //#endregion
+//#endregion
 
-// //#region  static-columns migration
+//#region  static-columns routes
+//#region @browser
+export const StaticColumnsServerRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
+export const StaticColumnsClientRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: () => {
+      if (StaticColumnsClientRoutes.length === 1) {
+        return '';
+      }
+      return StaticColumnsClientRoutes.find(r => r.path !== '')!.path!;
+    },
+  },
+  // PUT ALL ROUTES HERE
+  // @placeholder-for-routes
+  // @app-ts-generated
+  {
+    path: 'preview',
 
-// //#region @websql
-// @TaonMigration({
-//   className: 'UserMigration',
-// })
-// class UserMigration extends TaonBaseMigration {
-//   userController = this.injectRepo(User);
+    loadChildren: () =>
+      import('./app/preview/preview.routes').then(m => m.PreviewRoutes),
+  },
+];
+//#endregion
+//#endregion
 
-//   async up(): Promise<any> {
-//     const superAdmin = new User();
-//     superAdmin.name = 'super-admin';
-//     await this.userController.save(superAdmin);
-//   }
-// }
-// //#endregion
+//#region  static-columns app configs
+//#region @browser
+export const StaticColumnsAppConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(),
+    {
+      provide: TAON_CONTEXT,
+      useFactory: () => StaticColumnsContext,
+    },
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => StaticColumnsStartFunction,
+    },
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(StaticColumnsClientRoutes, withHashLocation()),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+};
 
-// //#endregion
+export const StaticColumnsServerConfig: ApplicationConfig = {
+  providers: [provideServerRendering(withRoutes(StaticColumnsServerRoutes))],
+};
 
-// //#region  static-columns context
-// var StaticColumnsContext = Taon.createContext(() => ({
-//   ...HOST_CONFIG['StaticColumnsContext'],
-//   contexts: { TaonBaseContext },
+export const StaticColumnsConfig = mergeApplicationConfig(
+  StaticColumnsAppConfig,
+  StaticColumnsServerConfig,
+);
+//#endregion
+//#endregion
 
-//   //#region @websql
-//   /**
-//    * In production use specyfic for this context name
-//    * generated migration object from  ./migrations/index.ts.
-//    */
-//   migrations: {
-//     UserMigration,
-//   },
-//   //#endregion
+//#region  static-columns entity
+@TaonEntity({ className: 'User' })
+class User extends TaonBaseAbstractEntity {
+  //#region @websql
+  @StringColumn()
+  //#endregion
+  name?: string;
 
-//   controllers: {
-//     UserController,
-//   },
-//   entities: {
-//     User,
-//   },
-//   database: true,
-//   // disabledRealtime: true,
-// }));
-// //#endregion
+  getHello(): string {
+    return `hello ${this.name}`;
+  }
+}
+//#endregion
 
-// //#region  static-columns start function
-// const StaticColumnsStartFunction = async (
-//   startParams?: Taon.StartParams,
-// ): Promise<void> => {
-//   await StaticColumnsContext.initialize();
-//   // @placeholder-for-contexts-init
+//#region  static-columns controller
+@TaonController({ className: 'UserController' })
+class UserController extends TaonBaseCrudController<User> {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  entityClassResolveFn = () => User;
 
-//   // INIT ALL ACTIVE CONTEXTS HERE
+  @GET()
+  helloWorld(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => 'hello world';
+    //#endregion
+  }
 
-//   //#region @backend
-//   if (
-//     startParams?.onlyMigrationRun ||
-//     startParams?.onlyMigrationRevertToTimestamp
-//   ) {
-//     process.exit(0);
-//   }
-//   //#endregion
+  @GET()
+  getOsPlatform(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => {
+      //#region @backend
+      return os.platform(); // for normal nodejs backend return real value
+      //#endregion
 
-//   //#region @backend
-//   console.log(`Hello in NodeJs backend! os=${os.platform()}`);
-//   //#endregion
-// };
-// //#endregion
+      return 'no-platform-inside-browser-and-websql-mode';
+    };
+    //#endregion
+  }
+}
+//#endregion
 
-// export default StaticColumnsStartFunction;
+//#region  static-columns migration
+
+//#region @websql
+@TaonMigration({
+  className: 'UserMigration',
+})
+class UserMigration extends TaonBaseMigration {
+  userController = this.injectRepo(User);
+
+  async up(): Promise<any> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.userController.save(superAdmin);
+  }
+}
+//#endregion
+
+//#endregion
+
+//#region  static-columns context
+var StaticColumnsContext = Taon.createContext(() => ({
+  ...HOST_CONFIG['StaticColumnsContext'],
+  contexts: { TaonBaseContext },
+
+  //#region @websql
+  /**
+   * In production use specyfic for this context name
+   * generated migration object from  ./migrations/index.ts.
+   */
+  migrations: {
+    UserMigration,
+  },
+  //#endregion
+
+  controllers: {
+    UserController,
+  },
+  entities: {
+    User,
+  },
+  database: true,
+  // disabledRealtime: true,
+}));
+//#endregion
+
+//#region  static-columns start function
+export const StaticColumnsStartFunction = async (
+  startParams?: Taon.StartParams,
+): Promise<void> => {
+  await StaticColumnsContext.initialize();
+  // @placeholder-for-contexts-init
+
+  // INIT ALL ACTIVE CONTEXTS HERE
+
+  //#region @backend
+  if (
+    startParams?.onlyMigrationRun ||
+    startParams?.onlyMigrationRevertToTimestamp
+  ) {
+    process.exit(0);
+  }
+  //#endregion
+
+  //#region @backend
+  console.log(`Hello in NodeJs backend! os=${os.platform()}`);
+  //#endregion
+};
+//#endregion
+
+export default StaticColumnsStartFunction;
--- src/app.vscode.ts
+++ src/app.vscode.ts
@@ -1,60 +1,59 @@
-// //#region imports
-// import { Utils } from 'tnp-core/src';
-// import { CommandType, ExecCommandType, executeCommand } from 'tnp-helpers/src';
-// import type { ExtensionContext } from 'vscode';
-// //#endregion
+import { Utils } from 'tnp-core/src';
+import { CommandType, executeCommand } from 'tnp-helpers/src';
+import type { ExtensionContext } from 'vscode';
 
-// const group = 'static-columns CLI essentials';
+const group = 'StaticColumns CLI essentials';
 
-// export const commands: CommandType[] = (
-//   [
-//     {
-//       title: 'hello world',
-//       exec: opt => {
-//         console.log('opt', opt);
-//         // opt.log.info('hello world');
-//       },
-//     },
-//     {
-//       title: 'hey!',
-//       exec: opt => {
-//         opt.log.info('hey!');
-//       },
-//     },
-//   ] as CommandType[]
-// ).map(c => {
-//   if (!c.command) {
-//     c.command = `extension.${Utils.camelize(c.title)}`;
-//   }
-//   if (!c.group) {
-//     c.group = group;
-//   }
-//   return c;
-// });
+export const commands: CommandType[] = (
+  [
+    {
+      title: 'hello world StaticColumns',
+      exec: ({ vscode }) => {
+        vscode.window.showInformationMessage('Hello World! StaticColumns');
+      },
+    },
+    {
+      title: 'hey StaticColumns! show platform',
+      exec: ({ vscode }) => {
+        vscode.window.showInformationMessage(
+          `StaticColumns platform is "${process.platform}"`,
+        );
+      },
+    },
+  ] as CommandType[]
+).map(c => {
+  if (!c.group) {
+    c.group = group;
+  }
+  if (!c.command) {
+    c.command = `extension.${Utils.camelize(c.group)}.${Utils.camelize(c.title)}`;
+  }
+  return c;
+});
 
-// export function activate(context: ExtensionContext) {
-//   for (let index = 0; index < commands.length; index++) {
-//     const {
-//       title,
-//       command = '',
-//       exec = '',
-//       options,
-//       isDefaultBuildCommand,
-//     } = commands[index];
-//     const sub = executeCommand(
-//       title,
-//       command,
-//       exec,
-//       options,
-//       isDefaultBuildCommand,
-//       context,
-//     );
-//     if (sub) {
-//       context.subscriptions.push(sub);
-//     }
-//   }
-// }
+export function activate(context: ExtensionContext): void {
+  for (let index = 0; index < commands.length; index++) {
+    const {
+      title = '',
+      command = '',
+      exec = '',
+      options,
+      isDefaultBuildCommand,
+    } = commands[index];
+    const sub = executeCommand(
+      title,
+      command,
+      exec,
+      options,
+      isDefaultBuildCommand,
+      context,
+    );
+    if (sub) {
+      context.subscriptions.push(sub);
+    }
+  }
+}
 
-// export function deactivate() {}
+export function deactivate(): void {}
 
-// export default { commands };
+export default { commands };
